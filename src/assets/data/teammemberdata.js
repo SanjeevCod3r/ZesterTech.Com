dc425568f74a2adc951ef7e84f3e58ace9d35006
Sanjeev Jaiswal <sanjeev1822.be21@chitkara.edu.in>
2025-08-01 12:05:54 +0530

    [Sanjeev Jaiswal] Add: All Done

--- src/assets/data/teammemberdata.js
+++ src/assets/data/teammemberdata.js
@@ -33,7 +33,7 @@ export const teamMembers = [
     {
       name: 'Ankush Kumar',
       position: 'Director & Invester',
-      bio: 'An accomplished investor with over 5 years of experience, actively funding 4 thriving companies and successfully operating a chain of 15+ retail outlets',
+      bio: 'Experienced investor funding 4 companies and running 15+ successful retail outlets',
       image: ankushimage,
       social: {
         linkedin: '#',
